c1c402e77d20422797e795b862494a0b2649ac63
Viet <103765155+VolLove@users.noreply.github.com>
2023-11-15 10:14:13 +0700

    update

--- example-app/resources/views/backend/header.blade.php
+++ example-app/resources/views/backend/header.blade.php
@@ -72,7 +72,7 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                     data-accordion="false">
                     <li class="nav-item">
-                        <a href="" class="nav-link">
+                        <a href="{{ route('dashboard') }}" class="nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>Dashboard
                             </p>
@@ -102,7 +102,14 @@
                     <li class="nav-item">
                         <a href="" class="nav-link">
                             <i class="nav-icon fas fa-table"></i>
-                            <p>
+                            <p>Nhãn hàng
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <i class="nav-icon fas fa-table"></i>
+                            <p>Loại hàng hóa
                             </p>
                         </a>
                     </li>
