d5d3fe537af8aff5ac27322e6874e751ab2c387e
VanDanh <tranvandanh157@gmail.com>
2023-11-18 05:08:30 +0700

    fix bug relative product

--- example-app/resources/views/fontend/product.blade.php
+++ example-app/resources/views/fontend/product.blade.php
@@ -213,7 +213,7 @@
                     <div class="col-6 col-md-4 col-lg-3 col-xl-2">
                         <div class="product product-5 text-center">
                             <figure class="product-media">
-                                <a href="product/{{$value['id']}}">
+                                <a href="{{$value['id']}}">
                                     <img src="{{ url('images/products/' . $value->image, []) }}" alt="Product image"
                                         class="product-image">
                                 </a>
