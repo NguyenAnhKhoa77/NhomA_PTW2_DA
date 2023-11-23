575cbf3489ae2126694292001153ef6688a8ae80
Hoaan <21211tt3903@mail.tdc.edu.vn>
2023-11-24 02:55:06 +0700

    completed profile

--- example-app/app/Http/Controllers/UserProfileController.php
+++ example-app/app/Http/Controllers/UserProfileController.php
@@ -2,16 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UserProfileController extends Controller
 {
     public function index()
     {
-        return view('fontend.account.profile');
+        $userInfo = Account::findOrFail(auth()->user()->id);
+        return view('fontend.account.profile', compact('userInfo'));
+    }
+
+    public function updateProfile(Request $request, Account $account)
+    {
+        $request->validate([
+            'name' => [
+                'required',
+                'max:255',
+            ],
+            'phone' => [
+                'required',
+                'regex:/^0[0-9]{9}$/',
+            ],
+            'address' => [
+                'required',
+                'max:255',
+            ],
+            'avatar' => [
+                'image',
+                'max:2048',
+            ],
+        ]);
+        $data = $request->all();
+        $userInfo = $data;
+        if ($request->file('avatar') != '') {
+            $destinationPath = 'backend/img/';
+            $userInfo['avatar'] = $request->file('avatar')->getClientOriginalName();
+            $file_old = $destinationPath . $userInfo['avatar'];
+            //code for remove old file
+            if ($file_old != '' && $file_old != null) {
+
+            } else {
+                unlink($file_old);
+            }
+
+            //upload new file
+            $file = $request->file('avatar');
+            $fileName = $file->getClientOriginalName();
+            $file->move($destinationPath, $fileName);
+
+            //for update in table
+        }
+        $account->update($userInfo);
+        return redirect()->back()->withSuccess('Change succeed!');
     }
 
     public function address()
