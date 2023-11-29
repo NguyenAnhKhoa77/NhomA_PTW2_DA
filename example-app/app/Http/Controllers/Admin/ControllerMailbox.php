9763de53f05ad1e3ec43ec88d044e49f670ddc79
VanDanh <tranvandanh157@gmail.com>
2023-11-30 03:07:02 +0700

    recreate mails, admin reply

--- example-app/app/Http/Controllers/Admin/ControllerMailbox.php
+++ example-app/app/Http/Controllers/Admin/ControllerMailbox.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use App\Models\User;
+use App\Models\Mails;
 use Illuminate\Http\Request;
 
 class ControllerMailbox extends Controller
@@ -21,6 +22,34 @@ class ControllerMailbox extends Controller
         $mails = Contact::find($id);
         $user = User::find($mails->user_id);
         return view('backend.mailbox.read-mail',compact('mails','user'));
+    }
+    
+    public function reply($id)
+    {
+        $mails = Contact::find($id);
+        $user = User::find($mails->user_id);
+        return view('backend.mailbox.reply',compact('mails','user'));
+    }
+    
+    public function replyForm($id, Request $request)
+    {
+        $request->validate([
+            'receiver' => 'required|int',
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+        //Add contact
+        $mailbox = new Mails([
+            'receive_user_id' => $request->input('receiver'),
+            'title' => $request->input('title'),
+            'content' => $request->input('content')
+        ]);
+        //Add thanh cong
+        if ($mailbox->save()) {
+            return back()->withSuccess('Gui thu thanh cong');
+        }
+        //Add that bai
+        return back()->withErrors('Gui thu that bai!');
     }
     
 }
