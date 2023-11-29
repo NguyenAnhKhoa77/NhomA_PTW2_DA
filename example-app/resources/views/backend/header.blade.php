c6c0011f35f9d7de88b86a186f5f061e92a7b8d3
QuocViet <103765155+VolLove@users.noreply.github.com>
2023-11-30 03:51:48 +0700

    fix

--- example-app/resources/views/backend/header.blade.php
+++ example-app/resources/views/backend/header.blade.php
@@ -280,7 +280,10 @@
                                 class="nav-link {{ Request::is('admin/coupons') || Request::is('admin/coupons/*') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-table"></i>
                                 <p>Phiếu giảm giá
-
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
                             <a href="{{ route('mailbox.mailbox') }}"
                                 class="nav-link {{ Route::currentRouteName() == 'comments.table' || Route::currentRouteName() == 'comments.create' || Route::currentRouteName() == 'comments.edit' ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-table"></i>
