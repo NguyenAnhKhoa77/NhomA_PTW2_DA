dda813ae8743883959d2657ea6ad0efcb8e5a9f4
adminlachalame <con@cc.vn>
2023-11-30 04:07:11 +0700

    commit-starf

---
+++ example-app/database/migrations/2023_11_29_204116_add_discount_to_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // Thêm cột discount và discounted_price
+            $table->decimal('discount', 5, 2)->nullable();
+            $table->decimal('discounted_price', 10, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
